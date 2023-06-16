3bce0c53e4498a22fdf8d2d1eac795256b391a16
joshvillarreal <joshv2017@gmail.com>
2023-06-16 10:23:08 -0400

    typo in test run

--- testRun.sh
+++ testRun.sh
@@ -11,8 +11,8 @@
 
 srun hostname
 echo ""
-which julia-1
+which julia
 echo ""
 echo "Running Julia Code"
-srun --unbuffered julia --project = "." -t 3 scan_hyperparameters_withcellnum.jl --data-directory=data/full_with_cellnumber --n-folds=2 --depth-range=3 5 --width-range=50 50 --depth-steps=3 --width-steps=2 --activation-functions=sigmoid --batch-size-range=1024 1024 --batch-size-steps=2 --learning-rate-range=0.001 0.001 --learning-rate-steps=2 --dropout-rate-range=0.0 0.0 --dropout-rate-steps=2 --n-epochs=25 --log-training-starts=true --log-training-loss=true --log-folds=true --gpu --cut-transmission
+srun --unbuffered julia --project="." -t 3 scan_hyperparameters_withcellnum.jl --data-directory=data/full_with_cellnumber --n-folds=2 --depth-range=3 5 --width-range=50 50 --depth-steps=3 --width-steps=2 --activation-functions=sigmoid --batch-size-range=1024 1024 --batch-size-steps=2 --learning-rate-range=0.001 0.001 --learning-rate-steps=2 --dropout-rate-range=0.0 0.0 --dropout-rate-steps=2 --n-epochs=25 --log-training-starts=true --log-training-loss=true --log-folds=true --gpu --cut-transmission
 echo ""
