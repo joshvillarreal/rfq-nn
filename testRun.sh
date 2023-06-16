8a37d41e56d2fea7f0a9c03d02461a4bb0cc5652
joshvillarreal <joshv2017@gmail.com>
2023-06-16 11:27:27 -0400

    fewer epochs on testrun, remove unneeded CUDA set version info line from scan

--- testRun.sh
+++ testRun.sh
@@ -14,5 +14,5 @@ echo ""
 which julia
 echo ""
 echo "Running Julia Code"
-srun --unbuffered julia --project="." -t 3 scan_hyperparameters_withcellnum.jl --data-directory=data/full_with_cellnumber --n-folds=2 --depth-range=3 5 --width-range=50 50 --depth-steps=3 --width-steps=2 --activation-functions=sigmoid --batch-size-range=1024 1024 --batch-size-steps=2 --learning-rate-range=0.001 0.001 --learning-rate-steps=2 --dropout-rate-range=0.0 0.0 --dropout-rate-steps=2 --n-epochs=25 --log-training-starts --log-training-loss --log-folds --gpu --cut-transmission
+srun --unbuffered julia --project="." -t 3 scan_hyperparameters_withcellnum.jl --data-directory=data/full_with_cellnumber --n-folds=2 --depth-range=3 5 --width-range=50 50 --depth-steps=3 --width-steps=2 --activation-functions=sigmoid --batch-size-range=1024 1024 --batch-size-steps=2 --learning-rate-range=0.001 0.001 --learning-rate-steps=2 --dropout-rate-range=0.0 0.0 --dropout-rate-steps=2 --n-epochs=10 --log-training-starts --log-training-loss --log-folds --gpu --cut-transmission
 echo ""
