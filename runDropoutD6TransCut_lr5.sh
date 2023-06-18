ba035a3ec79998bc6ca769a9ba5b108f11c8c747
joshvillarreal <joshv2017@gmail.com>
2023-06-18 12:00:30 -0400

    split up dropout rate scans into multiple jobs

---
+++ runDropoutD6TransCut_lr5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=rfq-nn-gpu-dropout-d6-lr5
+#SBATCH --output=res_%j.txt
+#SBATCH --error=err_%j.txt
+#
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=64GB
+#SBATCH --partition=submit-gpu
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-gpu=1
+
+srun hostname
+echo ""
+which julia
+echo ""
+echo "Running Julia Code"
+srun --unbuffered julia --project="." -t 2 scan_hyperparameters_withcellnum.jl --data-directory=data/full_with_cellnumber --n-folds=5 --depth-range=6 6 --width-range=100 100 --depth-steps=2 --width-steps=2 --activation-functions=sigmoid --batch-size-range=1024 1024 --batch-size-steps=2 --learning-rate-range=0.005 0.005 --learning-rate-steps=2 --dropout-rate-range=0.05 0.25 --dropout-rate-steps=5 --n-epochs=2500 --log-training-starts --log-training-loss --log-folds --gpu --cut-transmission
+echo ""
---
+++ runDropoutD7TransCut_lr3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=rfq-nn-gpu-dropout-d7-lr3
+#SBATCH --output=res_%j.txt
+#SBATCH --error=err_%j.txt
+#
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=64GB
+#SBATCH --partition=submit-gpu
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-gpu=1
+
+srun hostname
+echo ""
+which julia
+echo ""
+echo "Running Julia Code"
+srun --unbuffered julia --project="." -t 2 scan_hyperparameters_withcellnum.jl --data-directory=data/full_with_cellnumber --n-folds=5 --depth-range=7 7 --width-range=100 100 --depth-steps=2 --width-steps=2 --activation-functions=sigmoid --batch-size-range=1024 1024 --batch-size-steps=2 --learning-rate-range=0.003 0.003 --learning-rate-steps=2 --dropout-rate-range=0.0 0.25 --dropout-rate-steps=6 --n-epochs=2500 --log-training-starts --log-training-loss --log-folds --gpu --cut-transmission
+echo ""
